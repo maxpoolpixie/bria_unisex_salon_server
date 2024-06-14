4cc434b7f19f625000823b1e4e3c61ef77b8eb60
maxpoolpixie <chakri.medcv@gmail.com>
2024-06-14 01:40:10 -0700

    cron and fetch updated

--- utils/bookingUtilities.js
+++ utils/bookingUtilities.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch');
+const cron = require('node-cron');
+
 const formatDate = (date) => {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
