20c73a324b34fe847ea959dc1287a53c987cbeef
maxpoolpixie <chakri.medcv@gmail.com>
2024-06-24 21:28:18 +0600

    login system added

---
+++ controller/adminController.js
@@ -0,0 +1,19 @@
+const Admin = require("../model/adminSchema");
+
+const adminController = {
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const isExist = await Admin.findOne({ email: email, password: password });
+            if (!isExist) {
+                return res.json({ success: false })
+            }
+            res.json({ success: true })
+        } catch (error) {
+            res.json({ message: false })
+        }
+    }
+
+}
+
+module.exports = { adminController }
