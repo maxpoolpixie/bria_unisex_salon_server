e4190c8b4b79a0366e29c44b101a603ca8a072a9
maxpoolpixie <chakri.medcv@gmail.com>
2024-06-26 23:35:20 +0600

    push

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -14,7 +14,7 @@ function formatDate(date) {
     const year = date.getFullYear();
     return `${year}-${month}-${day}`;
 }
-
+console.log(1)
 function formatTime(date) {
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
